801224d788b50e2afb5b498f1a36fb88a54013af
nicolas-vdn <150355022+nicolas-vdn@users.noreply.github.com>
2023-11-18 11:39:09 +0000

    (feat): Added presence on refresh

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ const bot = new Discord.Client({
     intents: ["GuildMembers", "MessageContent", "Guilds"]
 })
 
+bot.on("ready", () => {
+    bot.user.setPresence({
+        status: "online",
+        activities: [{
+            name: `Rafraîchi à ${Date.now().toISOString().slice(11,16)}`
+        }]
+    })
+})
+
 bot.login(process.env.BOT_TOKEN)
